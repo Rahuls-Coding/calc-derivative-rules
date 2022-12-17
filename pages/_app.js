f04d885abbaab96ee272aedd8300bf6d5d1ebb8b
Rahul <rahulscodingworld@gmail.com>
2022-12-17 09:50:34 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
 import '../styles/globals.css'
+import Script from "next/script";
 import { Manrope } from '@next/font/google'
+
 
 const manrope = Manrope({
   subsets: ['latin'],
 });
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <main className={manrope.className}>
+      <Script 
+        strategy='lazyOload' 
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy='lazyOnload' id='1'>
+        {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+               page_path: window.location.pathname,
+             });  
+        `}
+      </Script>
       <Component {...pageProps} />
     </main>
   )
